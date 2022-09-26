cbf394d8c9509fc0601d073c4188014448a3a337
swisskill <wbrant@uwyo.edu>
2022-09-26 07:23:54 -0600

    frag made

---
+++ calculator/app/src/main/java/com/example/calculator/calc.java
@@ -0,0 +1,33 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link calc#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class calc extends Fragment {
+    public static calc newInstance(){
+        return new calc();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_tip, container, false);
+        return myView;
+
+    }
+}
